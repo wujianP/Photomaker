2b2db867eea9796800c135f1d0be739919b6e18c
wujianP <1051233700@qq.com>
2024-01-20 00:18:13 +0800

    com

--- run_main.sh
+++ run_main.sh
@@ -5,7 +5,7 @@ python main_real.py \
 --output='/DDN_ROOT/wjpeng/zoo/photomaker/output' \
 --input='/DDN_ROOT/wjpeng/zoo/photomaker/input/ym' \
 --exp-name='ym' \
---prompt="sci-fi, closeup portrait photo of an aisa man img in Superhero suit, strong muscle, face, high quality, film grain" \
+--prompt="sci-fi, closeup portrait photo of an aisa man img in superhero suit, draped in a cape, and wielding a longsword, strong muscle, face, high quality, film grain" \
 --neg-prompt="(asymmetry, worst quality, low quality, illustration, 3d, 2d, painting, cartoons, sketch), open mouth" \
---n-images=4
+--n-images=2
 
